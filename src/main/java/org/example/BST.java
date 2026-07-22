eac31262b04e74fa3d1c0b21b37d8cafba14a70c
Sahid Reza <sahidreza976@gmail.com>
2026-07-22 06:27:38 +0530

    Add print root to leaf function

--- src/main/java/org/example/BST.java
+++ src/main/java/org/example/BST.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class BST {
 
     static class Node{
@@ -116,6 +118,26 @@ public class BST {
         }
     }
 
+    public static void printRoot2Leaf(Node root , ArrayList<Integer>path){
+        if(root == null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left == null && root.right == null){
+          printPath(path);
+        }else {
+            printRoot2Leaf(root.left,path);
+            printRoot2Leaf(root.right,path);
+        }
+        path.remove(path.size()-1);
+    }
+    public static void printPath(ArrayList<Integer>path){
+        for(int i = 0;i<path.size();i++){
+            System.out.print(path.get(i) +" -> ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
         int values[] = {5,1,3,4,2,7};
@@ -134,10 +156,12 @@ public class BST {
         }else {
             System.out.println("Not Found");
         }
-        delete(root,5);
+      //  delete(root,5);
         System.out.print("Deleted inorder: ");
         inOrder(root);
         System.out.println();
         printInRange(root,2,5);
+        System.out.println();
+        printRoot2Leaf(root,new ArrayList<>());
     }
 }
